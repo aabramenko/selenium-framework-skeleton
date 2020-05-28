d9907949d2d39e52373f6f4a02b764152b7cef3e
Aleksei Abramenko <abramenko.alexey@gmail.com>
2020-05-29 00:54:18 +0300

    add git pull 32

--- docker/test-execution.sh
+++ docker/test-execution.sh
@@ -1,12 +1,9 @@
 echo "Checking if hub is ready: http://$HUB_HOST:4444/wd/hub/status"
 
-int n = 0
-while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ] && [ $n -lt 15 ]
+while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
   echo "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )"
 	sleep 1
-	n=$n+1
-	echo $n
 done
 
 mvn test -DSUITE=$SUITE \
